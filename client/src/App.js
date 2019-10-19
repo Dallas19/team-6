eedc7f1d954dfa261d44bfe11ccc694765bde753
Shabab Karim <skarim222@gmail.com>
2019-10-19 06:51:17 -0500

    Created the main view

--- client/src/App.js
+++ client/src/App.js
@@ -1,89 +1,34 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import React, { useState } from 'react'
+import { Container, Grid } from '@material-ui/core'
+import RoleContext from '../src/Context'
+import Header from './components/Header'
+import Filter from './components/Filter'
+import Graph from './components/Graph'
 
-// components
-import Chart from './components/Chart'
-
-const months = {
-	January: ['January', 'February', 'March'],
-	February: ['February', 'March', 'April'],
-	March: ['March', 'April', 'May'],
-	April: ['April', 'May', 'June'],
-	May: ['May', 'June', 'July'],
-	June: ['June', 'July', 'August'],
-	July: ['July', 'August', 'September'],
-	August: ['August', 'September', 'October'],
-	September: ['September', 'October', 'November'],
-	October: ['October', 'November', 'December'],
-	November: ['November', 'December', 'January'],
-	December: ['December', 'January', 'February']
-}
-
-const endpoint = ''
-class App extends React.Component {
-	datas
-	startMonth
-	loggedIn
-	strategy
-	indicator
-	programNames
-	partnerNames
-
-	getThreeMonths(startMonth) {
-		return months[startMonth]
-	}
-	updateData() {}
-
-	toggleLog() {
-		this.setState({ loggedIn: !this.loggedIn })
-	}
-
-	render() {
-		const checkbox = (
-			<div>
-				<input
-					type='checkbox'
-					onClick={this.toggleLog.bind(this)}
-				/>
-				<label>Checkbox</label>
-			</div>
-		)
-		const charts = () => {
-			let charts = []
-			for (let i = 0; i < this.datas.length; ++i) {
-				charts.push(
-					<Chart
-						data={this.datas[i]}
-						program={this.programNames[i]}
-						partner={this.partnerName[i]}
-						target={this.targets[i]}
-					/>
-				)
-			}
-			return charts
-		}
-
-		return (
-			<div>
+function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	return (
+		<div>
+			<RoleContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
 				<div>
-					<AppBar position='static'>
-						<Toolbar>
-							<Typography variant='title' color='inherit'>
-								Data Visualization Tool for United Way
-								of San Antonio
-							</Typography>
-							{checkbox}
-						</Toolbar>
-					</AppBar>
+					<Header />
 				</div>
-				<br />
-				<br />
-				{charts}
-			</div>
-		)
-	}
+				<div className='appBody' style={{ paddingTop: '10px' }}>
+					<Container maxWidth='lg'>
+						<Grid
+							container
+							direction='row'
+							justify='space-around'
+							alignItems='center'
+						>
+							<Filter />
+							<Graph />
+						</Grid>
+					</Container>
+				</div>
+			</RoleContext.Provider>
+		</div>
+	)
 }
 
 export default App
