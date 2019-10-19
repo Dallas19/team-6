eb386dc56ac738675f4e486ddc452dd6692b0038
Shabab Karim <skarim222@gmail.com>
2019-10-19 03:36:37 -0500

    Cleaned client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,112 +1,89 @@
-import React from 'react';
+import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 
-import logo from './logo.svg';
-import axios from 'axios';
 // components
 import Chart from './components/Chart'
-import './App.css';
 
 const months = {
-    January: ['January', 'February', 'March'],
-    February: ['February', 'March', 'April'],
-    March: ['March', 'April', 'May'],
-    April: ['April', 'May', 'June'],
-    May: ['May', 'June', 'July'],
-    June: ['June', 'July', 'August'],
-    July: ['July', 'August', 'September'],
-    August: ['August', 'September', 'October'],
-    September: ['September', 'October', 'November'],
-    October: ['October', 'November', 'December'],
-    November: ['November', 'December', 'January'],
-    December: ['December', 'January', 'February']
+	January: ['January', 'February', 'March'],
+	February: ['February', 'March', 'April'],
+	March: ['March', 'April', 'May'],
+	April: ['April', 'May', 'June'],
+	May: ['May', 'June', 'July'],
+	June: ['June', 'July', 'August'],
+	July: ['July', 'August', 'September'],
+	August: ['August', 'September', 'October'],
+	September: ['September', 'October', 'November'],
+	October: ['October', 'November', 'December'],
+	November: ['November', 'December', 'January'],
+	December: ['December', 'January', 'February']
 }
 
-const endpoint = "";
+const endpoint = ''
 class App extends React.Component {
-    datas;
-    startMonth;
-    loggedIn;
-    strategy;
-    indicator;
-    programNames;
-    partnerNames;
-    
-    getThreeMonths(startMonth) {
-        return months[startMonth]
-    }
-    updateData() {
-        axios.get(`${this.endpoint}?loggedIn=${this.loggedIn}&strategy=${this.strategy}&startMonth=${this.startMonth}&indicator=${this.indicator}`)
-            .then(res => {
-                for (let i = 0; i < res.data.length; ++i) {
-    	            
-                    let months = this.getThreeMonths(this.startMonth);
+	datas
+	startMonth
+	loggedIn
+	strategy
+	indicator
+	programNames
+	partnerNames
 
+	getThreeMonths(startMonth) {
+		return months[startMonth]
+	}
+	updateData() {}
 
-                    for (let j = 0; j < 3; ++j) {
-                        this.datas[i][months[j]+"19"] = res.data[i][months[j]+"19"];    	            
-                    }
+	toggleLog() {
+		this.setState({ loggedIn: !this.loggedIn })
+	}
 
+	render() {
+		const checkbox = (
+			<div>
+				<input
+					type='checkbox'
+					onClick={this.toggleLog.bind(this)}
+				/>
+				<label>Checkbox</label>
+			</div>
+		)
+		const charts = () => {
+			let charts = []
+			for (let i = 0; i < this.datas.length; ++i) {
+				charts.push(
+					<Chart
+						data={this.datas[i]}
+						program={this.programNames[i]}
+						partner={this.partnerName[i]}
+						target={this.targets[i]}
+					/>
+				)
+			}
+			return charts
+		}
 
-                    this.programNames[i] = res.data[i]["Program_name"];
-
-                    if (this.loggedIn)
-                    {
-                        this.programNames[i] = res.data[i]["Program_name"];
-                        this.partnerNames[i] = res.data[i]["Partner_name"];
-                    }
-
-                    this.annualTarget[i] = res.data[i]["AnnualTarget"]
-                }
-                
-            })
-    }
-
-
-    toggleLog() {
-        this.setState({loggedIn: !this.loggedIn})
-    }
-    
-    render() {
-        const checkbox = (
-            <div>
-              <input 
-                type="checkbox"
-                onClick={this.toggleLog.bind(this)} />
-              <label>Checkbox</label>
-            </div>
-        );
-        const charts = () => {
-            let charts = [];
-            for (let i = 0; i < this.datas.length; ++i) {
-                charts.push(<Chart data={this.datas[i]}
-                                   program={this.programNames[i]}
-                                   partner={this.partnerName[i]}
-                                   target={this.targets[i]}/>)
-    	    }
-            return charts
-        }
-        
-        return (
-            <div>
-              <div>
-                <AppBar position="static">
-                  <Toolbar>
-                    <Typography variant="title" color="inherit">
-                      Data Visualization Tool for United Way of San Antonio
-                    </Typography>
-                    {checkbox}
-                  </Toolbar>
-                </AppBar>
-              </div>
-              <br />
-              <br />
-              {charts}
-            </div>
-        )
-    }
+		return (
+			<div>
+				<div>
+					<AppBar position='static'>
+						<Toolbar>
+							<Typography variant='title' color='inherit'>
+								Data Visualization Tool for United Way
+								of San Antonio
+							</Typography>
+							{checkbox}
+						</Toolbar>
+					</AppBar>
+				</div>
+				<br />
+				<br />
+				{charts}
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import '../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries, XAxis, YAxis, HorizontalGridLines, VerticalGridLines} from 'react-vis';
-
+import React, { Component } from 'react'
+import {
+	XYPlot,
+	LineSeries,
+	XAxis,
+	YAxis,
+	HorizontalGridLines,
+	VerticalGridLines
+} from 'react-vis'
 
 class Chart extends Component {
+	data = []
 
-  data = [];
+	constructor(props) {
+		super(props)
+		let keys = Object.keys(props.data),
+			values = Object.values(props.data)
 
-  constructor (props) {
-    super(props);
-    let keys = Object.keys(props.data), 
-        values = Object.values(props.data);
-
-
-
-    for (let count =0; count < keys.length; count++)
-    {
-
-        this.data[count].x = keys[count];
-        this.data[count].y = values[count];
- 
-    }
-
-  }
-  render () {
-    /*const data = [
+		for (let count = 0; count < keys.length; count++) {
+			this.data[count].x = keys[count]
+			this.data[count].y = values[count]
+		}
+	}
+	render() {
+		/*const data = [
       {x: "ABC", y: 8},
       {x: "1", y: 5},
       {x: "2", y: 4},
@@ -38,19 +35,18 @@ class Chart extends Component {
       {x: "9", y: 0},
     ];
     */
-    return (
-      <div className="App">
-        <XYPlot xType='ordinal' height={300} width={1000}>
-
-          <VerticalGridLines/>
-          <HorizontalGridLines />
-          <XAxis />
-          <YAxis />
-          <LineSeries data={this.data} />
-        </XYPlot>
-      </div>
-    );
-  }
+		return (
+			<div className='App'>
+				<XYPlot xType='ordinal' height={300} width={1000}>
+					<VerticalGridLines />
+					<HorizontalGridLines />
+					<XAxis />
+					<YAxis />
+					<LineSeries data={this.data} />
+				</XYPlot>
+			</div>
+		)
+	}
 }
 
-export default Chart;
+export default Chart
