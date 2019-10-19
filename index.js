7637dc9647f989dfd0b6760ac3b014faa1ae161c
Shabab Karim <skarim222@gmail.com>
2019-10-18 21:06:25 -0500

    Added a filereader

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+const reader = require('./utils/FileReader')
+
 app.use(bodyParser.json())
 	.use(morgan(':method :url :status :response-time'))
 	.use(cors())
 
 app.get('/', (req, res) => {
-	res.json('Hello World')
+	res.json('This is the default path')
+})
+
+app.get('/readFile', async (req, res) => {
+	// reading the first 1000 bytes
+	const unitedWayRows = await reader('./United-Way.csv', 0, 999)
+	res.json(unitedWayRows)
 })
 
 app.listen(5000, () => console.log('Listening on Port 5000'))
