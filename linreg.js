ecd0195d87aeccfeb2f1f69120e30048d26234dd
tothepowerofn <nathanedu@icloud.com>
2019-10-19 06:12:18 -0500

    getRawData: Take json row values and make an (x,y) dataset

--- linreg.js
+++ linreg.js
@@ -14,12 +14,27 @@ module.exports = {
             sum_yy += Y[i]*Y[i];
             sum_xy += X[i]*Y[i];
         } 
-        out[0] = (Y.length*sum_xy - sum_x*sum_y)/(Y.length * sum_xx - sum_x * sum_x)
-        out[1] = (sum_y - out[0]*sum_x)/Y.length
-        return out
+        out[0] = (Y.length*sum_xy - sum_x*sum_y)/(Y.length * sum_xx - sum_x * sum_x);
+        out[1] = (sum_y - out[0]*sum_x)/Y.length;
+        return out;
     },
     //takes a set of json row block
     getRawData: function(jsonObjArr){
-
+        var X_vals = new Array();
+        var Y_vals = new Array(); 
+        for(let i = 0; i < jsonObjArr.length; ++i){
+            //hardcoded to include for 3 months
+            //TODO: Hardcoding should take different month names
+            //Point 1
+            X_vals.push(0);
+            Y_vals.push(jsonObjArr[i]['Unique Count July 2019']);
+            //Point 2
+            X_vals.push(1);
+            Y_vals.push(jsonObjArr[i]['Unique Count August 2019']);
+            //Point 3
+            X_vals.push(2);
+            Y_vals.push(jsonObjArr[i]['Unique Count September 2019']);
+        }
+        return {X: X_vals, Y: Y_vals};
     }
   };
