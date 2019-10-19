b93037529bc9725c5c5e2e7a7e62d79835f5107a
Shabab Karim <skarim222@gmail.com>
2019-10-18 23:59:19 -0500

    Added endpoint to populate db

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 
 const mongoConnection = require('./config')
 const reader = require('./utils/FileReader')
+const performance = require('./models/performance')
 
 app.use(bodyParser.json())
 	.use(morgan(':method :url :status :response-time'))
@@ -25,13 +26,34 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => console.log('db connected!'))
 
 app.get('/', (req, res) => {
-	res.json('This is the default path')
+	res.json({
+		isLoggedIn: false
+	})
 })
 
+app.get('/login', (req, res) => {
+	res.json({
+		isLoggedIn: true
+	})
+})
+
+// only once to populate the db
 app.get('/readFile', async (req, res) => {
 	// reading the first 73 complete rows of data
 	const unitedWayRows = await reader('./United-Way.csv', 0, 40000)
-	res.json(unitedWayRows)
+	const completeUnitedWayRows = unitedWayRows.slice(0, 73)
+	completeUnitedWayRows.forEach(async row => {
+		try {
+			// prettier-ignore
+			await performance.replaceOne({ "Sort Order": row['Sort Order'] }, row, { upsert: true }, err => {
+					if (err) throw err
+				}
+			)
+		} catch (err) {
+			console.log(err)
+		}
+	})
+	res.json(completeUnitedWayRows)
 })
 
 app.listen(5000, () => console.log('Listening on Port 5000'))
