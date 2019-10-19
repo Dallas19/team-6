861d9f38aef9369a7fc857f9cb8efd872d7b0497
charje <charles.b.jackson@protonmail.com>
2019-10-19 01:17:01 -0500

    change to class

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
+// components
 import NavBar from './components/NavBar'
+import Chart from './components/Chart'
 import './App.css';
 
-function App() {
-  return (
-   <NavBar /> 
-  )
+
+const endpoint = "";
+class App extends React.Component {
+    data;
+    isLoggedIn;
+    strategy;
+    indicator;
+    
+    updateData() {
+        axios.get(`${endpoint}?loggedIn=${loggedIn}&strategy=${strategy}&indicator=${indicator}`)
+            .then(res => {
+                this.setState({data: res.data})
+            })
+    }
+
+    
+    render() {
+        return (
+            <div>
+              <NavBar />
+              <br />
+              <br />
+              <Chart data={data}/>
+            </div>
+        )
+    }
 }
 
 export default App;
