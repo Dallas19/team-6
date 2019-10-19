d96664b5d0d3ebf3437ae0b50c2db750fc1d80b7
tothepowerofn <nathanedu@icloud.com>
2019-10-19 06:00:24 -0500

    linreg: fix small syntax errors

--- linreg.js
+++ linreg.js
@@ -10,16 +10,16 @@ module.exports = {
         for (let i = 0; i < Y.length; ++i){
             sum_x += X[i];
             sum_y += Y[i];
-            sum_xx += X[i]*X[i]
-            sum_yy += Y[i]*Y[i]
-            sum_xy = X[i]*Y[i]
+            sum_xx += X[i]*X[i];
+            sum_yy += Y[i]*Y[i];
+            sum_xy += X[i]*Y[i];
         } 
         out[0] = (Y.length*sum_xy - sum_x*sum_y)/(Y.length * sum_xx - sum_x * sum_x)
         out[1] = (sum_y - out[0]*sum_x)/Y.length
         return out
     },
     //takes a set of json row block
-    getRawData: function(jsonPoints){
-        
+    getRawData: function(jsonObjArr){
+
     }
   };
