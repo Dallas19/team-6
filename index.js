4671e0518c44b639d1c6acc3055ad669c6f45ed2
Shabab Karim <skarim222@gmail.com>
2019-10-18 20:39:10 -0500

    Imported modules

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
+
+app.use(bodyParser.json())
+	.use(morgan(':method :url :status :response-time'))
+	.use(cors())
 
 app.get('/', (req, res) => {
 	res.json('Hello World')
